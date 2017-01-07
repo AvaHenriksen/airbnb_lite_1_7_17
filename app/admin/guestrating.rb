3995ace0399502e68dfa55a94299870a9c495385
Raghu Betina <raghu@firstdraft.com>
2017-01-07 22:26:00 +0000

    Whitelist columns in Guestrating for ActiveAdmin

--- app/admin/guestrating.rb
+++ app/admin/guestrating.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Guestrating do
 
+ permit_params :host_id, :guest_id, :booking_id, :body, :rating
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
